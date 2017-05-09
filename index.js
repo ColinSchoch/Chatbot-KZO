aa3f225d852f94acc5d7a74a927c7464da0405fe
Colin Schoch <colinschoch@me.com>
2017-05-09 20:11:23 +0200

    Divided a Codeline

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ app.listen((process.env.PORT || 3000));
 
 // Server frontpage
 app.get('/', function (req, res) {
-    res.send('This is the Chatbot for the KZO, if you want to talk to me come visit me on Facebook. Please follow this link: https://www.facebook.com/Chatbot-1835882586663460/?ref=aymt_homepage_panel'); // Answer
+    res.send('This is the Chatbot for the KZO, if you want to talk to me come visit me on Facebook. ' +
+    ' Please follow this link: https://www.facebook.com/Chatbot-1835882586663460/?ref=aymt_homepage_panel'); // Answer
 });
 
 app.get('/test', function (req, res) {
