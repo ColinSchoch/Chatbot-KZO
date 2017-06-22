ecf670bb5840a8a32bf42493bab729a9d71eece7
Colin Schoch <colinschoch@me.com>
2017-06-22 22:34:55 +0200

    timetable added for each class

--- index.js
+++ index.js
@@ -42,13 +42,13 @@ app.post('/webhook', function (req, res) {
 
           if (userInput === "random") {
             sendMessage(event.sender.id, {text: getRandom()});
-          }
-
-          else if (userInput === "fluchen") {
+          } else if (userInput === "fluchen") {
             sendMessage(event.sender.id, {text: "https://www.youtube.com/watch?v=LosO2ifzLRE"});
+          } else if ( checkIfTimetable(userInput) ) {
+            sendMessage(event.sender.id, {text: getTimeTableUrl(userInput) });
           }
 
-          else {
+           else {
             sendMessage(event.sender.id, {text: "Mirror: " + event.message.text});
           }
 
@@ -63,6 +63,25 @@ function getRandom() {
   var num = Math.random() *100;
   return Math.floor(num);
 }
+
+function checkIfTimetable(userInput) {
+  var classString = userInput.slice(-3);
+  var classes = ["a6" , "an6", "c6a", "c6b", "c6c", "m6a", "m6b", "n6a", "n6b", "nm6", "w6a", "w6b", "ac5", "an5", "c5a", "c5b", "c5c", "m5a", "m5b", "n5a", "n5b", "w5b", "wn5", "a4" , "c4a", "c4b", "c4c", "c4d", "cw4", "m4a", "m4b", "n4a", "n4b", "n4c", "w4" , "a3" , "c3a", "c3b", "c3c", "m3a", "m3b", "n3a", "n3b", "n3c", "n3d", "w3", "u2a", "u2b", "u2c", "u2d", "u2e", "u2f", "u2g", "u1a", "u1b", "u1c", "u1d", "u1e", "u1f"];
+  var exists = classes.indexOf(classString) >= 0 ;
+
+  return userInput.search("stundenplan") >= 0 && exists === true ;
+
+}
+function getTimeTableUrl(userInput) {
+  var classString = userInput.slice(-3);
+  var classes = ["a6" , "an6", "c6a", "c6b", "c6c", "m6a", "m6b", "n6a", "n6b", "nm6", "w6a", "w6b", "ac5", "an5", "c5a", "c5b", "c5c", "m5a", "m5b", "n5a", "n5b", "w5b", "wn5", "a4" , "c4a", "c4b", "c4c", "c4d", "cw4", "m4a", "m4b", "n4a", "n4b", "n4c", "w4" , "a3" , "c3a", "c3b", "c3c", "m3a", "m3b", "n3a", "n3b", "n3c", "n3d", "w3", "u2a", "u2b", "u2c", "u2d", "u2e", "u2f", "u2g", "u1a", "u1b", "u1c", "u1d", "u1e", "u1f"];
+  var keys    = [2020 , 2021 , 2017 , 2018 , 2019 , 2023 , 2026 , 2024 , 2027 , 2022 , 2025 , 2028 , 2006 , 2007 , 2010 , 2013 , 2016 , 2009 , 2012 , 2011 , 2014 , 2015 , 2008 , 1994 , 1998 , 2001 , 2003 , 2005 , 1995 , 1997 , 2000 , 1999 , 2002 , 2004 , 1996 , 1987 , 1988 , 1990 , 2034 , 2031 , 2033 , 1989 , 1991 , 1992 , 1993 , 2032 , 1979 , 1980 , 1981 , 1982 , 1983 , 1984 , 1985 , 1974 , 1975 , 1976 , 1977 , 1978 , 1986];
+  var classindex = classes.indexOf(classString);
+  var key = keys[classindex];
+  var today = date.getFullYear().toString() + "-" + (date.getMonth()+1).toString() + "-" + date.getDate().toString();
+  return "https://intranet.tam.ch/kzo/public/public-schedule?onlyTable=0&returnEntity=class&entityId=" + key + "&date=" + today + "&showBasicTimetable=0&width=99.99%25";
+}
+
 // generic function sending messages
 function sendMessage(recipientId, message) {
     request({
