219fe9faf2b851b07acc27eb91fa4304b0109829
Colin Schoch <colinschoch@me.com>
2017-10-21 21:20:27 +0200

    polishes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var request = require('request');
 var app = express();
 
-app.use(bodyParser.urlencoded({extended: false})); // TODO: lookup
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 3000));
 
@@ -37,7 +37,7 @@ var importantWordsZimmer       = ["darf", "rein", "wo", "wie", "viel", "Zeit", "
 var importantWordsStundenplan  = ["wo", "sehe", "zimmer", "wie", "stunde", "ausfällt"];
 var importantWordsMensa        = ["wie", "teuer", "essen", "menüplan", "wo", "funktioniert", "rabattkarte"];
 var importantWordsLehrer       = ["wo", "sehe", "welcher", "lehrer","unterichtet", "wann", "schule", "hat", "mit", "problem", "was", "in", "welchem", "zimmer", "ist", "jetzt", "klasse"];
-var importantWordsOnline       = ["wo", "sehe", "ist", "stundenplan", "studmail", "was", "e-mail", "lehrer"];
+var importantWordsOnline       = ["wo", "sehe", "ist", "stundenplan", "studmail", "was", "e-mail", "lehrer", "email", "online"];
 var importantWordsSlsUndMatur  = ["wann", "infos"];
 
 // Server(Website) frontpage
@@ -52,10 +52,10 @@ app.get('/test', function (req, res) {
 
 app.get('/Datennutzungsbestimmung', function (req, res) {
     res.send("Datennutzungsbestimmung KZO-Bot vom 25.09.2017 <br /> <br /> "  +
-"Diese Seite dient zu ihrer Information über die Datennutzungsbestimmungen des KZO-Bots. <br /> <br /> " +
-"Ihre persönlichen Daten und Informationen werden in keiner Form von dem KZO-Chatbot gespeichert. Nur der Name ihres Facebook-Profils ist für den Urheber dieser Seite sichtbar. Dieser wird aber niemanden weitergegeben. " +
-"Die Fragen die sie an den KZO-Bot stellen sind für den Betreiber sichtbar und werden eventuell zur Weiterentwicklung des KZO-Bots verwendet.<br /> <br /> " +
-"Ansonsten sind sie aber für niemanden sichtbar und werden auch an niemanden weitergegeben. Die Fragen werden auch extern nicht gespeichert, sondern sind nur im Facebook-Verlauf. <br /> <br /> " +
+"Diese Seite dient zu ihrer Information über die Datennutzungsbestimmungen des KZO-Bots.<br /> <br /> " +
+"Ihre persönlichen Daten und Informationen werden in keiner Form vom KZO-Chatbot gespeichert. Nur der Name ihres Facebook-Profils ist für den Urheber dieser Seite sichtbar. Dieser wird aber niemanden weitergegeben." +
+"Die Fragen die sie an den KZO-Bot stellen sind für den Betreiber sichtbar und werden eventuell zur Weiterentwicklung des KZO-Bots verwendet.<br /><br />" +
+"Ansonsten sind sie aber für niemanden sichtbar und werden auch an niemanden weitergegeben oder gespeichert, abgesehen vom Facebook-Verlauf.<br /><br />" +
 "Es ist dem Urheber vorbehalten diese Datennutzungsbestimmungen bei Bedarf zu ändern."
 );
 });
@@ -227,92 +227,89 @@ function generateAnswer(relevantWords, estimatedCategory) {
       answer = "Ein neues Absenzenheft kannst du im Sekretariat holen. Dabei musst du aber entweder das volle Absenzenheft mitbringen, oder wenn du es verloren hast, muss der Klassenlehrer unterschreiben, dass du es verloren hast.";
     }
     else if (relevantWords.includes("unterschreiben")) {
-      answer = "Das Absenzenheft muss von deinen Eltern und von jedem Lehrer unterschrieben werden, bei dem du gefehlt hast. Ausserdem muss der Klassenlehrer ganz am Schluss unterschreiben. Sobald du 18 bist darfst dann du anstelle deiner Eltern unterschreiben, vorher jedoch nicht!";
+      answer = "Das Absenzenheft muss von deinen Eltern und von jeder Lehrperson unterschrieben werden, bei welcher du gefehlt hast. Ausserdem muss der Klassenlehrer ganz am Schluss unterschreiben. Sobald du 18 bist darfst dann du anstelle deiner Eltern unterschreiben, vorher jedoch nicht!";
     }
     else if (relevantWords.includes("wie")&& relevantWords.includes("lange")&& relevantWords.includes("zeit")) {
-      answer = ":O"; // richtige antwort einfügen
+      answer = "Um alle seine Absenzen unterschreiben zu lassen hat man 2 Wochen Zeit.";
     }
     else if (relevantWords.includes("wie")&& relevantWords.includes("urlaubsgesuch")&& relevantWords.includes("zeit")) {
       answer = "Um ein Urlaubsgesuch zu bekommen muss man die Unterschrift spätestens eine Woche vor dem Datum des Gesuchs holen.";
     }
-
-    // else noch hinzufügen
   }
   else if (estimatedCategory === "zimmer"){
     if (relevantWords.includes("darf")&& relevantWords.includes("rein")){
-      answer = "Ja in freie Zimmer darf man rein.";
+      answer = "Freie Zimmer darf man jederzeit betreten.";
     }
     else if (relevantWords.includes("wo")&& relevantWords.includes("sportplatz")){
       answer = "Den Sportplatz findet man gerade hinter der Eishalle. Um dort hinzukommen muss man bei den Turnhallen der Strasse nach rechts folgen und dann links über den Platz gehen, bis man zu Eishalle kommt. Dort muss man dann einfach noch rechts um die Eishalle gehen und dann kommt man zum Sportplatz.";
     }
     else if (relevantWords.includes ("wie")&& relevantWords.includes("viel")&& relevantWords.includes("zeit")&& relevantWords.includes("sportplatz")&& relevantWords.includes("mehr")){
-      answer = "Grundsätzlich beginnt eine Sportlektion 2 Minuten später als normale Lektionen, aber wenn man auf dem Sportplatz Sport hat kommen nochmals 3 Min uten hinzu. Das heisst eine Lektion auf dem Sportplatz beginnt 5 Minuten später als eine normale Schullektion.";
+      answer = "Grundsätzlich beginnt eine Sportlektion 2 Minuten später als normale Lektionen, aber wenn man auf dem Sportplatz Sport hat, kommen nochmals 3 Minuten hinzu. Das bedeutet, eine Lektion auf dem Sportplatz beginnt 5 Minuten später als eine normale Schullektion.";
     }
     else if (relevantWords.includes ("wo")&& (relevantWords.includes("turnhalle") || relevantWords.includes("sporthalle"))&& relevantWords.includes("a")){
-      answer = "Turnhalle A findet man wenn man von der Freitreppe aus zu den Turnhallen läuft und dann gleich nach der Treppe rechts und dann runter geht. "+
-               "Die Umkleidekabinen findet man, wenn man noch eine Treppe runter geht und links den Gang entlang.";
+      answer = "Turnhalle A findet man wenn man von der Freitreppe aus zu den Turnhallen läuft und dann gleich nach der Treppe rechts und dann hinunter geht. "+
+               "Die Umkleidekabinen findet man wenn man noch eine Treppe hinunter geht und danach links den Gang entlang geht.";
     }
     else if (relevantWords.includes("wo")&& (relevantWords.includes("turnhallen") || relevantWords.includes("sporthallen"))){
-      answer = "Die Turnhallen findet man wenn man von der Freitreppe aus links am Brunnen vorbeigeht. Dann geht man bis zur Doppeltüre rechts, für die man keine Treppe runter muss. Dort hinein findet man die Turnhallen.";
+      answer = "Die Turnhallen findet man wenn man von der Freitreppe aus links am Brunnen vorbeigeht. Dann geht man bis zur Doppeltüre rechts, für die man keine Treppe runter muss. Dort befinden sich die Turnhallen.";
     }
     else if (relevantWords.includes ("wo")&& (relevantWords.includes("turnhalle") || relevantWords.includes("sporthalle"))&& relevantWords.includes("b")){
-      answer = "Turnhalle B findet man wenn man von der Freitreppe aus richtung Turnhallen geht und dann durch den Haupteingang läuft. "+
-               "Wenn man die Treppe runterläuft und dann nach rechts geht ist die erste Turnhalle die Turnhalle B.";
+      answer = "Turnhalle B findet man wenn man von der Freitreppe aus in Richtung Turnhallen geht und dann durch den Haupteingang läuft. "+
+               "Wenn man die Treppe hinuntergeht und dann nach rechts geht, ist die erste Turnhalle die Turnhalle B.";
     }
     else if (relevantWords.includes ("wo")&& (relevantWords.includes("turnhalle") || relevantWords.includes("sporthalle"))&& relevantWords.includes("c")){
-      answer = "Turnhalle C findet man wenn man von der Freitreppe aus richtung Turnhallen geht und dann nach unten geht. Wenn man die Treppe runterläuft und dann nach links geht ist die erste Turnhalle die Turnhalle C.";
+      answer = "Turnhalle C findet man wenn man von der Freitreppe aus richtung Turnhallen geht und dann nach unten geht. Wenn man die Treppe hinuntergeht und dann nach links geht, ist die erste Turnhalle die Turnhalle C.";
     }
     else if (relevantWords.includes ("wo")&& (relevantWords.includes("turnhalle") || relevantWords.includes("sporthalle"))&& relevantWords.includes("d")){
-      answer = "Turnhalle D findet man wenn man von der Freitreppe aus richtung Turnhallen geht und dann durch den Haupteingang läuft. Wenn man die Treppe runterläuft und dann nach rechts geht muss man an einer Turnhalle vorbei gehen und die hintere ist dann die Turnhalle D.";
+      answer = "Turnhalle D findet man wenn man von der Freitreppe aus in Richtung Turnhallen geht und dann durch den Haupteingang läuft. Wenn man die Treppe hinuntergeht und dann nach rechts geht, muss man an einer Turnhalle vorbei gehen und die hintere ist dann die Turnhalle D.";
     }
     else if (relevantWords.includes ("wo")&& (relevantWords.includes("turnhalle") || relevantWords.includes("sporthalle"))&& relevantWords.includes("e")){
-      answer = "Turnhalle E findet man wenn man von der Freitreppe aus richtung Turnhallen geht und dann durch den Haupteingang läuft. Wenn man die Treppe runterläuft und dann nach links geht an der Turnhalle C vorbei kommt man zur Turnhalle E.";
+      answer = "Turnhalle E findet man wenn man von der Freitreppe aus in Richtung Turnhallen geht und dann durch den Haupteingang läuft. Wenn man die Treppe hinuntergeht und dann nach links an der Turnhalle C vorbeigeht, kommt man zur Turnhalle E.";
     }
   }
   else if (estimatedCategory === "stundenplan"){
     if (relevantWords.includes("wo")&& relevantWords.includes("sehe")&& relevantWords.includes("zimmer")){
-      answer = "Das Zimmer sieht man auf dem Stundenplan jeweils auf der unteren Zeile die Zahl ganz rechts.";
+      answer = "Das Zimmer sieht man auf dem Stundenplan. Es ist jeweils auf der unteren Zeile die Zahl ganz rechts.";
     }
     else if (relevantWords.includes ("stunde")&& relevantWords.includes("sehe")&& relevantWords.includes("ausfällt")){
-      answer = "Ob eine Stunde ausfällt sieht man auf dem Stundenplan. Wenn eine Lektion rot durchgestrichen ist heisst dass, das die Stunde ausfällt.";
+      answer = "Ob eine Stunde ausfällt, sieht man auf dem Stundenplan. Wenn eine Lektion rot durchgestrichen ist, fällt diei Stunde aus.";
     }
   }
   else if (estimatedCategory === "mensa"){
     if (relevantWords.includes("wie")&& relevantWords.includes("teuer")&& relevantWords.includes("essen")){
-      answer = "Ein warmes Menü mit Salat kostet in der Mensa 9 Fr. ohne Salat kostet es 8 Fr.";
+      answer = "Ein warmes Menü mit Salat kostet in der Mensa 9 Fr.; ohne Salat kostet es 8 Fr.";
     }
     else if (relevantWords.includes("wo")&& relevantWords.includes("menüplan")){
-      answer = "Den Menüplan sieht man bei der Mensa unten wenn man die Treppe zur Mensa runtergeht auf der linken Seite bei der grossen Tafel.";
+      answer = "Den Menüplan sieht man bei der Mensa unten, wenn man die Treppe zur Mensa hinuntergeht, auf der linken Seite bei der grossen Tafel.";
     }
     else if (relevantWords.inludes("wie")&& relevantWords.includes("funktioniert")&& relevantWords.includes("rabattkarte")){
-      answer = "Die Mensakarte funktioniert so dass man zuerst für die volle Karte, also 12 Menüs und streicht dann immer einen Punkt ab wenn man ein Menü kauft. Wenn man seine 12 Menüs gekauft hat erhält man dann noch ein 13. Menü gratis!!!";
+      answer = "Die Mensakarte funktioniert so, dass man zuerst für die volle Karte, also 12 Menüs bezahlt, und dann jedesmal wenn man ein Menü kauft einen Punkt abstreicht. Wenn man seine 12 Menüs gekauft hat erhält man dann noch ein 13. Menü gratis!!!";
     }
   }
   else if (estimatedCategory === "lehrer"){
     if (relevantWords.includes("wo")&& relevantWords.includes("sehe")&& relevantWords.includes("welcher")&& relevantWords.includes("lehrer")&& relevantWords.includes("unterichtet")){
-      answer = "Man sieht alle Lehrer und ihre Unterichtszeiten auf einem Plan neben dem Lehrerzimmer. Das Lehrerzimmer findet man wenn man von dem Foyer aus richtung Aula läuft ist es die letze Tür vor dem Seiteneingang zur KZO.";
+      answer = "Man sieht alle Lehrer und ihre Unterichtszeiten auf dem Plan neben dem Lehrerzimmer. Das Lehrerzimmer findet man wenn man von dem Foyer aus in Richtung Aula läuft. Es ist es die letze Tür vor dem Seiteneingang zur KZO.";
     }
     else if (relevantWords.includes("wo")&& relevantWords.includes("sehe")&& relevantWords.includes("wann")&& relevantWords.includes("welcher")&& relevantWords.includes("lehrer")&& relevantWords.includes("schule")&& relevantWords.includes("hat")){
-      answer = "Beim Lehrerzimmer hat es einen grossen Plan bei dem man von jedem Lehrer sieht wann er wo Untericht hat.";
+      answer = "Beim Lehrerzimmer hat es einen grossen Plan wo man von jeder Lehrperson sieht wann diese wo unterrichtet.";
     }
     else if (relevantWords.includes("klasse")&& relevantWords.includes("hat")&& relevantWords.includes("mit")&& relevantWords.includes("lehrer")&& relevantWords.includes("problem")&& relevantWords.includes("was")){
-      answer = "Am besten Versucht ihr zuerst mit dem Lehrer über euer Problem zu sprechen. Falls das nicht weiter hilf kontaktiert ihr am besten euren Klassenlehrer und besprecht mit ihm was ihr jetzt am besten als nächstes machen solltet.";
+      answer = "Am besten zuerst mit der betroffenen Lehrperson über das Problem sprechen. Falls das nicht hilft, den Klassenlehrer kontaktieren und mit ihm/ihr die nächsten Schritte besprechen.";
     }
   }
   else if (estimatedCategory === "online"){
-    if (relevantWords.includes("wo")&& relevantWords.includes("sehe")&& relevantWords.includes("online")&& relevantWords.includes("stundenplan")){
-      answer = "Unter https://intranet.tam.ch/kzo/ könnt ihr euch anmelden und dann unter der Leiste Stundenplan seht ihr euren jeweiligen Stundenplan mit allenfalls gestrichenen Stunden." +
-              ' Um auf diese Seite zu kommen könnt ihr auch gleich diesem <a href="https://intranet.tam.ch/kzo/">Link</a> folgen.';
+    if (relevantWords.includes("wo")&& relevantWords.includes("online")&& relevantWords.includes("stundenplan")){
+      answer = "Unter https://intranet.tam.ch/kzo/ kann man sich anmelden und dann unter der Leiste 'Stundenplan' den jeweiligen Stundenplan mit allenfalls gestrichenen Stunden einsehen.";
     }
     else if (relevantWords.includes("wo")&& relevantWords.includes("studmail")){
-      answer = 'Die Studmail findest du wenn du dich auf <a href="https://intranet.tam.ch/kzo/">dieser</a> Seite anmeldest. Dort siehst du dann oben rechts einen Briefumschlag. Wenn du den anklickts kommst du zu der Studmail.';
+      answer = 'Die Studmail findest du wenn du dich im Intranet, "https://intranet.tam.ch/kzo/", anmeldest. Über den Briefumschlag oben rechts kommst du zu der Studmail.';
     }
-    else if (relevantWords.includes("was")&& relevantWords.includes("email")&& relevantWords.includes("lehrer")){
-      answer = "Die E-Mail der Lehere ist immer vorname.nachname@kzo.ch. Dasselbe gilt auch für die Schüler, ausser dass nach dem @ noch ein studmail hinzukommt. Zum Beispiel so: mike.kobelt@studmail.kzo.ch ";
+    else if (relevantWords.includes("was")&& relevantWords.includes("e-mail") || relevantWords.includes("email")&& relevantWords.includes("lehrer")){
+      answer = "Die E-Mail der Lehrpersonen ist immer vorname.nachname@kzo.ch. Dasselbe gilt auch für die Schüler, ausser dass nach dem @ noch ein studmail hinzukommt. Zum Beispiel so: mike.kobelt@studmail.kzo.ch ";
     }
   }
   if (answer === "") {
-    answer = "Sorry ich habe deine Frage nicht verstanden. Bitte beachte, dass ich nur Hochdeutsch verstehe. Falls du deine Frage in Hochdeutsch gestellt hast und ich deine Frage immer noch nicht verstehe, versuche die Frage anders zu formulieren. Wenn auch das nichts nützt kann es sein, dass ich auf deine Frage keine Antwort weiss. Ich bitte um Entschuldigung.";
+    answer = "Sorry, ich habe deine Frage nicht verstanden. Bitte beachte, dass ich nur Hochdeutsch verstehe. Falls du deine Frage in Hochdeutsch gestellt hast, versuche die Frage anders zu formulieren. Achte bitte auch auf eine korrekte Rechtschreibung. Wenn auch das nichts nützt kann es sein, dass ich auf deine Frage keine Antwort weiss. Ich bitte um Entschuldigung.";
   }
   return answer;
 }
